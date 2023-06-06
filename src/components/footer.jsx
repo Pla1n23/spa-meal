17b5269fc8e5be17360f6a6127e18736a1cc39a8
Max Faterin <faterin01032004@gmail.com>
2023-06-06 23:44:07 +0300

    third commit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
         <div className="footer-copyright ">
             <div className="container">
                 © {new Date().getFullYear()} Kayumov E. ; Faterin M. ; Kulesha K. 
-                <a className="grey-text text-lighten-4 right" href="#!">Repository</a>
+                <a className="grey-text text-lighten-4 right" target="blank" href="https://github.com/Pla1n23/spa-meal">Repository</a>
             </div>
         </div>
     </footer>
